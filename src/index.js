4eb54cb3cffb76cfc135c2ff3897f718eaaa2164
HACKERA316 <ademmurat1616@gmail.com>
2022-11-10 16:47:24 +0300

    update file user:@hackera316

--- src/index.js
+++
@@ -1,5 +0,0 @@
-export function add(a, b) {
-    return a + b;
-}
-
-console.log(add(10,10));
---
+++ src/ındex.js
@@ -0,0 +1,7 @@
+export default function catUI(){
+    class CatUi extends HTMLElement{
+
+    }
+    customElements.define("cat-ui",CatUi);
+    
+}
